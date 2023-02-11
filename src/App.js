482e83c0d3f37205305112692954d8cb5d6a8ba8
Dominic Myers <annoyingmouse@gmail.com>
2023-02-11 12:40:43 +0000

    Trying to commit a change to a file in a git repository, but the commit fails with the following error:

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './assets/style/App.scss';
 import { useState } from 'react';
 
 function App() {
+  const [showEvents, setShowEvents] = useState(true);
   const [events, setEvents] = useState([
     {
       title: "Mario's birthday bash",
@@ -20,6 +21,14 @@ function App() {
   return (
     <div className="App">
       <h2>Upcoming events</h2>
+      <div>
+        <button type="button"
+                onClick={() => setShowEvents(false)}>Hide events</button>
+      </div>
+      <div>
+        <button type="button"
+                onClick={() => setShowEvents(true)}>Show events</button>
+      </div>
       {events.map((event, index) => (
         <div key={event.id}>
           <h3>{index} - {event.title}</h3>
