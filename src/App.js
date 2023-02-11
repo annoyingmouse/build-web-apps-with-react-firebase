a9656008207cdf86686d3c4991479c4e9bab01cf
Dominic Myers <annoyingmouse@gmail.com>
2023-02-11 10:35:15 +0000

    Why we need state

--- src/App.js
+++ src/App.js
@@ -1,27 +1,18 @@
-import logo from './assets/images/vector/logo.svg';
 import './assets/style/App.scss';
 
 function App() {
+  let name = 'mario'
+
+  const handleClick = () => {
+    name = 'luigi'
+    console.log(name)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src="./assets/images/vector/logo.svg"
-             className="App-logo"
-             alt="logo" />
-        <img src={logo}
-             className="App-logo"
-             alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a className="App-link"
-           href="https://reactjs.org"
-           target="_blank"
-           rel="noopener noreferrer" >
-          Learn React & Firebase
-        </a>
-      </header>
-    </div>
+      <h1>My name is {name}</h1>
+      <button onClick={handleClick}>Change name</button>
+    </div>  
   );
 }
 
