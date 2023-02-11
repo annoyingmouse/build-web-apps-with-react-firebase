b94905692a07ee0da420642f42f9039665882ab3
Dominic Myers <annoyingmouse@gmail.com>
2023-02-11 10:21:09 +0000

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './assets/images/vector/logo.svg';
+import './assets/style/App.scss';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src="./assets/images/vector/logo.svg"
+             className="App-logo"
+             alt="logo" />
+        <img src={logo}
+             className="App-logo"
+             alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+        <a className="App-link"
+           href="https://reactjs.org"
+           target="_blank"
+           rel="noopener noreferrer" >
+          Learn React & Firebase
         </a>
       </header>
     </div>
