72fc6e951d2330cd751a90c16f753b8c43f9a3a8
Dominic Myers <annoyingmouse@gmail.com>
2023-02-12 07:59:33 +0000

    controlled inputs

--- src/components/NewEventForm/NewEventForm.js
+++ src/components/NewEventForm/NewEventForm.js
@@ -1,25 +1,34 @@
 import styles from './NewEventForm.module.scss'
+import { useState } from 'react'
 
 export const NewEventForm = () => {
   const [title, setTitle] = useState('')
+  const [date, setDate] = useState('')
 
-  const handleChange = (e) => {
-    setTitle(e.value)
+  const resetForm = () => {
+    setTitle('')
+    setDate('')
   }
-  
+
   return (
     <form className={styles.form}>
       <label>
         <span>Event Title:</span>
-        <input type="text" onChange={handleChange}/>
+        <input type="text"
+               value={title}
+               onChange={e => setTitle(e.target.value)}/>
       </label>
       <label>
         <span>Event Date:</span>
-        <input type="date"/>
+        <input type="date"
+               value={date}
+               onChange={e => setDate(e.target.value)}/>
       </label>
       <button type="submit">
         Submit
       </button>
+      <p>title - {title}, date - {date}</p>
+      <p onClick={resetForm}>Reset the form</p>
     </form>
   )
 }
